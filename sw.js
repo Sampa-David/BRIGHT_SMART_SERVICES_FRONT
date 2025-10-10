96221ae8992380228e915d18d60dba1af1223299
Sampa-David <davidjosiassampa@gmail.com>
2025-10-10 01:48:37 +0100

    Initial commit: Frontend implementation with service modals

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'clarity-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/main.css',
+  '/assets/vendor/bootstrap/css/bootstrap.min.css',
+  '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+  '/assets/js/main.js',
+  '/assets/vendor/aos/aos.css',
+  '/assets/vendor/aos/aos.js',
+  '/assets/img/logo.webp'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
